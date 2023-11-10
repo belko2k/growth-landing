a99c6f1a4bc6926163e300e412153faa137eff57
Lovro Belovič <131977099+belko2k@users.noreply.github.com>
2023-11-10 18:53:09 +0100

    faq section

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const slider = document.querySelector('.testimonials-wrapper');
+const faqContainer = document.querySelector('.faq-content');
+
 
 let pos = { left: 0, x: 0 };
 
@@ -35,3 +37,50 @@ const mouseUpHandler = function () {
 };
 
 slider.addEventListener('mousedown', mouseDownHandler);
+
+// FAQ ACCORDION
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', (e) => {
+        const groupQuestion = e.target.closest('.faq-question');
+
+        if (!groupQuestion) return;
+
+        const group = groupQuestion.parentElement;
+        const groupAnswer = group.querySelector('.faq-answer');
+        const icon = groupQuestion.querySelector('i');
+        const divider = group.querySelector('.divider');
+
+        // Toggle icon
+        icon.classList.toggle('fa-xmark');
+        icon.classList.toggle('fa-plus');
+
+        // Toggle answer
+        groupAnswer.classList.toggle('open');
+
+        // Toggle data-type attribute
+        if (groupAnswer.classList.contains('open')) {
+            divider.setAttribute('data-type', 'dark');
+        } else {
+            divider.setAttribute('data-type', 'light');
+        }
+
+        // Close other groups
+        const otherGroups = faqContainer.querySelectorAll('.faq-group');
+        otherGroups.forEach((otherGroup) => {
+            if (otherGroup !== group) {
+                const otherGroupAnswer = otherGroup.querySelector('.faq-answer');
+                const otherIcon = otherGroup.querySelector('.faq-question i');
+                const otherDivider = otherGroup.querySelector('.divider');
+
+                otherGroupAnswer.classList.remove('open');
+                otherIcon.classList.remove('fa-xmark');
+                otherIcon.classList.add('fa-plus');
+                otherDivider.setAttribute('data-type', 'light');
+            }
+        });
+    });
+});
+
